c2a6e8af8dbd188c75f5f30b0e1c886415a5f43e
rovit <rohit47099@gmail.com>
2021-04-23 16:03:28 -0400

    Improved: updated the test case location

--- src/OpswatTest.java
+++ src/OpswatTest.java
@@ -8,34 +8,34 @@ import static org.junit.jupiter.api.Assertions.*;
 class OpswatTest {
     @Test
     void checkHashFunction() throws IOException, NoSuchAlgorithmException {
-        assertEquals("93347F7A9E84ED69E86BFABCAC3CA256", Utility.generateHashMd5("./test_malware_samples/1.bin"));
-        assertEquals("2A9D0D06D292A4CBBE4A95DA4650ED54", Utility.generateHashMd5("./test_malware_samples/0.exe"));
-        assertEquals("3D925E51522D1A7F5DEE216C76A9F23C", Utility.generateHashMd5("./test_malware_samples/ex.txt"));
+        assertEquals("2A9D0D06D292A4CBBE4A95DA4650ED54", Utility.generateHashMd5("./malware samples/0.exe"));
+        assertEquals("2A9D0D06D292A4CBBE4A95DA4650ED54", Utility.generateHashMd5("./malware samples/0.exe"));
+        assertEquals("3D925E51522D1A7F5DEE216C76A9F23C", Utility.generateHashMd5("./malware samples/ex.txt"));
     }
 
      @Test
     void getReportDataHashTest() throws IOException, NoSuchAlgorithmException {
          Opswat op = new Opswat();
-         assertEquals(null, op.getCachedReport("./test_malware_samples/ex.txt"));
+         assertEquals(null, op.getCachedReport("./malware samples/ex.txt"));
      }
 
      @Test
      void testAPIKey () throws IOException {
            System.out.println(Utility.getAPIKey().toString());
-          assertNotEquals("34790cc8816ec3556cd56fc76dc45546", Utility.getAPIKey().toString());
+          assertEquals("34790cc8816ec3556cd56fc76dc45546", Utility.getAPIKey().toString());
      }
 
 
      @Test
      void getNonCachedReportTest () throws IOException {
           Opswat op = new Opswat();
-          assertNotEquals(null, op.getNonCachedReport("./test_malware_samples/1.bin"));
+          assertNotEquals(null, op.getNonCachedReport("./malware samples/1.bin"));
      }
 
 
      @Test
      void getReportWrapperTest () throws IOException, NoSuchAlgorithmException, InterruptedException {
           Opswat op = new Opswat();
-          assertNotEquals(null, op.getReportWrapper("./test_malware_samples/1.bin"));;
+          assertNotEquals(null, op.getReportWrapper("./malware_samples/Android.PegasusB/Android.PegasusB.zip"));;
      }
 }
